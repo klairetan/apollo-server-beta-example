744373c1b7cd3cf2f18413e7f6e62a85a1d2ea67
Klaire Tan <klaire.tan@yale.edu>
2018-06-18 11:22:40 -0700

    Setup app w Apollo Server beta for Engine onboarding test

---
+++ index.js
@@ -0,0 +1,27 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+let count = 0;
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: (root, args, context) => {
+      count++;
+      console.log({ count });
+      return 'Hello world!';
+    },
+  },
+};
+
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`)
+});
